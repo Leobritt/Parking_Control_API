b4396c553bec426bceecb93e76cb4acd862e020e
Leobritt <leobritto160@gmail.com>
2023-05-29 22:46:13 -0300

    Adição método delete

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -87,5 +88,18 @@ public class ParkingSpotController {
 		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> delete(@PathVariable (value = "id") UUID id){
+		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+		
+		if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not found!");
+		}
+		/*deletando registro*/
+		parkingSpotService.delete(parkingSpotModelOptional.get());
+		
+		return ResponseEntity.status(HttpStatus.OK).body("Parking spot deleted!");
+		
 	}
 }
--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -13,48 +13,53 @@ import jakarta.transaction.Transactional;
 
 /*camada referente a regra de negocios e comunicação entre controller e o repository*/
 @Service
-/*depois criar uma interface e implemntar nesta classe*/
+/* depois criar uma interface e implemntar nesta classe */
 public class ParkingSpotService {
-	
-	
-	/*Ponto de injeção do repository dentro do controller*/
-	
-	final ParkingSpotRepository  parkingSpotRepository;
+
+	/* Ponto de injeção do repository dentro do controller */
+
+	final ParkingSpotRepository parkingSpotRepository;
 
 	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
 		this.parkingSpotRepository = parkingSpotRepository;
 	}
-	 
-	/*Usando o JPA*/
-	/*Método CREATE*/
-	@Transactional /*Garante o rollback*/
-	 public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {	 
+
+	/* Usando o JPA */
+	/* Método CREATE */
+	@Transactional /* Garante o rollback */
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
 		return parkingSpotRepository.save(parkingSpotModel);
-	 }
-	/*Método READ*/
-	@Transactional
-	public List<ParkingSpotModel> findAll(){
-		return parkingSpotRepository.findAll();
-	}
-	/*Método READ by id*/
-	@Transactional
-	public Optional <ParkingSpotModel> findById(UUID id) {
-		return parkingSpotRepository.findById(id);
 	}
 
-	 /*é necessario delcarar este método dentro da Interface Repository, pois o JPA não possui pronto*/
-    public boolean existsByLicensePlateCar(String licensePlateCar) {
-        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
-    }
+	/* Método READ */
+	public List<ParkingSpotModel> findAll() {
+		return parkingSpotRepository.findAll();
+	}
+
+	/* Método READ by id */
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
+	@Transactional
+	/* Método DELETE */
+	public void delete(ParkingSpotModel parkingSpotModel) {	
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+
+	/*
+	 * é necessario delcarar este método dentro da Interface Repository, pois o JPA
+	 * não possui pronto
+	 */
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
+		return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	}
 
 	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
 		return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
 	}
 
-    public boolean existsByApartmentAndBlock(String apartment, String block) {
-        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
-    }
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	}
 
-	 
-	 
 }
